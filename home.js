e1f2f661cb8bc6ed7ace3afba0a5e39760c29936
omer-farouq <omerfarouqstd@gmail.com>
2022-02-24 10:08:47 +0600

    adding all

---
+++ home.js
@@ -0,0 +1,18 @@
+
+
+function evenify_all(nums){
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+    if(num % 2 == 0){
+        console.log(num, ': is even number');
+        }
+    else{
+        console.log(num*2,': is odd number');
+        }      
+    }
+}
+nums = [5, 12, 89, 19, 8];
+evenify_all(nums);
+
+friends_age = [13, 17, 19, 20, 18];
+evenify_all(friends_age);
